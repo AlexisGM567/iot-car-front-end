288f9b3700dd4caffc43c4da227d12cacfac9c1c
AlexisGM567 <169068260+AlexisGM567@users.noreply.github.com>
2024-05-23 11:10:09 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -4,7 +4,7 @@ let respuesta = document.getElementById("respuesta");
 function callApiRequest() {
 
     // Make a request for a user with a given ID
-    axios.get('http://18.116.62.160/iot-car-control/back-end/apis/getRegistro.php')
+    axios.get('http://13.59.114.81/iot-car-control/back-end/apis/getRegistro.php')
         .then(function (response) {
             // handle success
             console.log(response);
